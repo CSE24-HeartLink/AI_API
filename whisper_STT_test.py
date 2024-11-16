87a8e94c17082e33c99ddc9d88d4115370316fa9
jihyeyu33 <jihyeyu33@gmail.com>
2024-11-16 16:17:17 +0900

    whisper API 테스트, 파일 수정

--- whisper_STT_test.py
+++ whisper_STT_test.py
@@ -2,13 +2,13 @@ from openai import OpenAI
 
 client = OpenAI()
 
-audio_file = open("data/채용면접_샘플_01.wav", "rb")
-transcript = client.audio.transcriptions.create(
-    file=audio_file,
-    model="whisper-1",
-    language="ko",
-    response_format="text",
-    temperature=0.0,
+with open(r"C:\Users\user\Desktop\CSE2024\AI_API\audio_test_1116.m4a", "rb") as audio_file:
+    transcript = client.audio.transcriptions.create(
+        file=audio_file,
+        model="whisper-1",
+        language="ko",
+        response_format="text",
+        temperature=0.0,
 )
 
 print(transcript)
